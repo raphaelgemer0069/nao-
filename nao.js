0af8abb65d8a82e044fe27300b3509eb190f3cb5
raphaelgemer0069 <raphael.correa.moraes@escola.pr.gov.br>
2026-02-20 07:47:53 -0300

    Create nao.js

---
+++ nao.js
@@ -0,0 +1,23 @@
+// criar bolinha
+let raio =7.5;
+let xbolinha = 100;
+let ybolinha = 200;
+let diametro =vraio * 2;
+//velocidade da bolinha
+let velocidadexbolinha = 6;
+let velocidadeybolinha = 6;
+// minha raquete
+let xraquete = 5;
+let yraquete = 150;
+// raquete oponente
+let xraqueteoponente = 585;
+let yraqueteoponente = 150;
+// placar 
+let meuspontos = 0;
+let pontosoponete = 0;
+function setup (){
+  createcanvas(600,400)
+}
+function draw(){
+  
+}
